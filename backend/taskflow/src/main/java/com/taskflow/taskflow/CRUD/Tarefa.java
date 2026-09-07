c8966685aa8ec78c33530d0abdb01fbe9ea77c67
LauraBargas <lauradiasbargas@gmail.com>
2026-09-06 22:56:35 -0300

    CRUD inicializado

---
+++ backend/taskflow/src/main/java/com/taskflow/taskflow/CRUD/Tarefa.java
@@ -0,0 +1,89 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+enum statusTarefa {
+    PENDENTE,
+    EM_ANDAMENTO,
+    CONCLUIDA
+}
+@Entity // define que essa classe é uma entidade do BD
+@Table(name = "tarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idtarefa;
+
+    @Column(name = "nomeprojeto", nullable = false) // Mapeia para a coluna 'nomeTarefa'
+    private String nometarefa;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "idusuario", nullable = false)
+      private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "idprojeto", nullable = false)
+     private Projeto projeto;
+    
+    @Enumerated(EnumType.ORDINAL)
+    @Column (name = "status", nullable = false)
+    private statusTarefa status;
+
+    public statusTarefa getStatus() {
+         return status;
+    }
+
+    public String getDescricao() {
+         return descricao;
+    }
+
+    public long getIdTarefa() {
+         return idtarefa;
+    }
+
+    public String getNomeTarefa() {
+         return nometarefa;
+    }
+
+    public Usuario getUsuario() {
+    return usuario;
+    }
+
+   public Projeto getProjeto() {
+    return projeto;
+   }
+
+
+    public void setStatus(statusTarefa status) {
+     this.status = status;
+    }
+
+    public void setIdTarefa(long idTarefa) {
+     this.idtarefa = idTarefa;
+    }
+
+    public void setNomeTarefa(String nomeTarefa) {
+     this.nometarefa = nomeTarefa;
+    }
+
+    public void setDescricao(String descricao) {
+     this.descricao = descricao;
+    }
+    public void setUsuario(Usuario usuario) {
+    this.usuario = usuario;
+    }
+    public void setProjeto(Projeto projeto) {
+    this.projeto = projeto;
+    }
+}
